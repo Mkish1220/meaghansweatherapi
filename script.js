31deeaeb89c2daa258db68e3bc74247eb4cc61fe
Meaghan Kish <106093711+Mkish420@users.noreply.github.com>
2022-07-09 11:49:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var apikey = "192aa32e12efeef9298dceab17715f5a";
+
+
+
+
+
+
 //search//
 function search() {
     var search_text = document.getElementById("search_text").value;
@@ -15,7 +22,19 @@ function search() {
 
 //activity3 mod 6//
 
-var submitButton = document.getElementById('submit-button');
+var submitButton = document.getElementById('searchBtn');
+
+
+submitButton.addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    var input = document.getElementById('cityInput').value;
+    currentWeather(input)
+  //console.log(input)
+
+
+})
 
 function getApi() {
   // fetch request gets a forecast of the weather
@@ -28,26 +47,26 @@ function getApi() {
     .then(function (data) {
       console.log(data)
       //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
+    //   for (var i = 0; i < data.length; i++) {
+    //     // Creating elements, tablerow, tabledata, and anchor
+    //     var createTableRow = document.createElement('tr');
+    //     var tableData = document.createElement('td');
+    //     var link = document.createElement('a');
 
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
+    //     // Setting the text of link and the href of the link
+    //     link.textContent = data[i].html_url;
+    //     link.href = data[i].html_url;
 
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableData.appendChild(link);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
-    });
+    //     // Appending the link to the tabledata and then appending the tabledata to the tablerow
+    //     // The tablerow then gets appended to the tablebody
+    //     tableData.appendChild(link);
+    //     createTableRow.appendChild(tableData);
+    //     tableBody.appendChild(createTableRow);
+    //   }
+     });
 }
 
-fetchButton.addEventListener('click', getApi);
+//fetchButton.addEventListener('click', getApi);
 
 
 //grab the search content//
@@ -62,8 +81,6 @@ fetchButton.addEventListener('click', getApi);
 // }
 //functionality for fetching weather//
 //functionality for rendering content//
-const apikey = "192aa32e12efeef9298dceab17715f5a";
-
 var todaysWeather = $("#todaysWeather");
 var today = $("#today");
 var fiveDay = $("#fiveDay");
@@ -71,46 +88,68 @@ var search = $("#search");
 var cities = [];
 
 
-var savedCities = localStorage.getItem("cities");
+var savedCities = localStorage.getItem("cities") || [];
 if (savedCities.length > 0) {
     cities = savedCities;
 }
 
- function(displayWeather) {
-    fetch(
-        "https://api.openweathermap.org/data/2.5/direct?q="
-        + search.val()
-        + "&appid="
-        + this.apikey
-    ).then((data) => this.geolocation(data))
-        .then((data) => {
-            todaysWeather.fetchedWeather(data);
-        });
-}
-getLocation: function(data) {
-    var let = data.[0].lat;
-    var lng = data.[0].lon;
-    return { lat, lon };
-}
-var weather = {
-    apikey: "192aa32e12efeef9298dceab17715f5a",
+ function currentWeather (city) {
+     var url= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`
 
-    fetchWeather: function (a) {
+    fetch(url)
+    .then(res => res.json())
+        .then((data) => {
+            console.log(data)
+            var card = document.createElement("div");
+            var cityName = document.createElement("h3");
+            var tempEl = document.createElement("p");
+
+
+            cityName.textContent = data.name;
+            tempEl.textContent = "Temperature: " + data.main.temp;
+
+            card.append(cityName, tempEl);
+
+            var renderEl = document.getElementById("currentRender");
+            console.log(card)
+            renderEl.append(card);
+
+
+        });
+
+        
+
+
+
+
+}
+ function getLocation(data) {
+    console.log(data)
+    // var let = data.[0].lat;
+    // var lng = data.[0].lon;
+    // return { lat, lon };
+
+
+
+
+    var apikey =  "192aa32e12efeef9298dceab17715f5a"
+
+  
         fetch(
             "https://api.openweathermap.org/data/2.5/wonecall?late="
-            + a.lat
+            + lat
             + "&lon="
-            + a.lon
-            + units="imperial"
+            + lon
+            + "units=imperial"
             + "&appid="
-        + this.apikey
+        + apikey
         )
             .then((response) => {
                 return response.json();
             })
             .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-        return data;
+    }
+  //  displayWeather: function (data) {
+    //    return data;
 
-     });
+     //}
